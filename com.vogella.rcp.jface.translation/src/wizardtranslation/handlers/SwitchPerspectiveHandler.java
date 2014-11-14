a4f013bb0ba3435dbc0349dbc2135dfdf0bea6cd
Simon Scholz <simon.scholz@vogella.com>
2014-11-14 16:53:21 +0100

    Also added the save and restore perspective handling in the sampel app
    Change-Id: Ib307b58e1b468fc3d0a4cfdd6aba36eb47f7cd10
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

---
+++ com.vogella.rcp.jface.translation/src/wizardtranslation/handlers/SwitchPerspectiveHandler.java
@@ -0,0 +1,30 @@
+ 
+package wizardtranslation.handlers;
+
+
+import java.util.List;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
+import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
+import org.eclipse.e4.ui.workbench.modeling.EModelService;
+import org.eclipse.e4.ui.workbench.modeling.EPartService;
+
+public class SwitchPerspectiveHandler {
+	@Execute
+	public void execute(EModelService modelService, EPartService partService,
+			MWindow window) {
+		MPerspective activePerspective = modelService
+				.getActivePerspective(window);
+
+		List<MPerspective> perspectives = modelService.findElements(window,
+				null, MPerspective.class, null);
+
+		for (MPerspective perspective : perspectives) {
+			if (!perspective.equals(activePerspective)) {
+				partService.switchPerspective(perspective);
+			}
+		}
+	}
+		
+}
