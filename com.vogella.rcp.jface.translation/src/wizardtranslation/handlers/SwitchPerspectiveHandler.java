cbce2efcd5e7833fd0257852cffdae2c304b4721
Simon Scholz <simon.scholz@vogella.com>
2014-11-20 13:37:03 +0100

    Added a command parameter for perspective switch
    Change-Id: I1f9de11f78a89690cd34dae0ea43e9216c05cb8c
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- com.vogella.rcp.jface.translation/src/wizardtranslation/handlers/SwitchPerspectiveHandler.java
+++ com.vogella.rcp.jface.translation/src/wizardtranslation/handlers/SwitchPerspectiveHandler.java
@@ -2,9 +2,10 @@
 package wizardtranslation.handlers;
 
 
-import java.util.List;
+import javax.inject.Named;
 
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.ui.MUIElement;
 import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
@@ -13,20 +14,18 @@ import org.eclipse.e4.ui.workbench.modeling.EPartService;
 public class SwitchPerspectiveHandler {
 	@Execute
 	public void execute(EModelService modelService, EPartService partService,
-			MWindow window) {
+			MWindow window,
+			@Named("com.vogella.rcp.jface.translation.commandparameter.perspectiveid") String id) {
 		MPerspective activePerspective = modelService
 				.getActivePerspective(window);
 
-		List<MPerspective> perspectives = modelService.findElements(window,
-				null, MPerspective.class, null);
 
-		// as we only have two perspectives simply switch to the non active
-		// perspective.
-		for (MPerspective perspective : perspectives) {
-			if (!perspective.equals(activePerspective)) {
-				partService.switchPerspective(perspective);
-			}
+		MUIElement find = modelService.find(id, window);
+		if (find == null || activePerspective.equals(find)) {
+			return;
 		}
+
+		partService.switchPerspective((MPerspective) find);
 	}
 		
 }
