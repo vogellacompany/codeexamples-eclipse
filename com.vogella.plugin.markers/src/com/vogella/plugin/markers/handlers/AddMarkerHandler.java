c4419609fb18a5498720917487405cf68137d9b1
Simon Scholz <simon.scholz@vogella.com>
2016-11-25 19:02:59 +0100

    Added a com.vogella.plugin.markers plugin
    Change-Id: I723dfc208f9ca77c02f7a97a54deb08d2ccc4a49
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

---
+++ com.vogella.plugin.markers/src/com/vogella/plugin/markers/handlers/AddMarkerHandler.java
@@ -0,0 +1,42 @@
+package com.vogella.plugin.markers.handlers;
+
+import javax.inject.Named;
+
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.core.services.adapter.Adapter;
+import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.jface.viewers.IStructuredSelection;
+
+
+public class AddMarkerHandler {
+
+	@Execute
+	public void execute(@Named(IServiceConstants.ACTIVE_SELECTION) IStructuredSelection selection, Adapter adapter) {
+
+		if (selection == null || selection.isEmpty()) {
+			return;
+		}
+
+		Object firstElement = selection.getFirstElement();
+		IResource resource = adapter.adapt(firstElement, IResource.class);
+
+		if (resource != null) {
+			writeMarkers(resource);
+		}
+
+	}
+
+	private void writeMarkers(IResource resource) {
+		try {
+			IMarker marker = resource.createMarker(IMarker.TASK);
+			marker.setAttribute(IMarker.MESSAGE, "This is a task");
+			marker.setAttribute(IMarker.PRIORITY, IMarker.PRIORITY_HIGH);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+}
