da50bf5176d7ea9d10d708e5262ecb6fdd7b0579
Lars Vogel <Lars.Vogel@vogella.com>
2017-02-17 12:18:16 +0100

    Adds CTabFolder for SWT examples to repo

---
+++ com.vogella.swt.widgets/src/com/vogella/swt/widgets/ctabfolder/CTabFolderExample.java
@@ -0,0 +1,41 @@
+package com.vogella.swt.widgets.ctabfolder;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+public class CTabFolderExample {
+    public static void main(String[] args) {
+        Display display = new Display();
+        Shell shell = new Shell(display);
+
+        shell.setLayout(new GridLayout());
+        // SWT.BOTTOM to show at the bottom
+        CTabFolder folder = new CTabFolder(shell, SWT.TOP);
+        GridData data = new GridData(SWT.FILL, 
+                SWT.FILL, true, true,
+                2, 1);
+        folder.setLayoutData(data);
+        CTabItem cTabItem1 = new CTabItem(folder, SWT.NONE);
+        cTabItem1.setText("Tab1");
+        CTabItem cTabItem2 = new CTabItem(folder, SWT.NONE);
+        cTabItem2.setText("Tab2");
+        CTabItem cTabItem3 = new CTabItem(folder, SWT.NONE);
+        cTabItem3.setText("Tab3");
+        CTabItem cTabItem4 = new CTabItem(folder, SWT.NONE);
+        cTabItem4.setText("Tab4");
+
+        shell.pack();
+        shell.open();
+        while (!shell.isDisposed()) {
+            if (!display.readAndDispatch()) {
+                display.sleep();
+            }
+        }
+    }
+}
